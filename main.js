194109ba98cc2f0207479be79abbf4a699bf058c
chamath1991 <chamath.erplanka@gmail.com>
2024-02-24 19:23:31 +0530

    creating library first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function emailValidation(email){
+
+    let regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regExp.test(email);
+}
+
+function capitalizeFirstWord(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export{
+    emailValidation,
+    capitalizeFirstWord
+}
